ddf04d28e2b746649c47fba7f4254576f183f9ae
TudorFernea <fernea.tudor8853@gmail.com>
2020-09-14 16:49:09 +0300

    added move function

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+def move(t1, t2):
+    t2.append(t1.pop())
+
 def solve(t1, t2, t3, num_disks):
     if num_disks == 3:
         move(t1,t3) #
+        render()    #
         move(t1,t2) #
+        render()    #
         move(t3,t2) #
+        render()    #
         move(t1,t3) #change all these calls somewhat? looks kinda ugly/repetitive
+        render()    #if not then put render() inside move? but then I have a function that does 2 separate things :\
         move(t2,t1) #
+        render()    #
         move(t2,t3) #
+        render()    #
         move(t1,t3) #
+        render()    #
+
     if num_disks > 3:
         solve(t1,t3,t2, num_disks - 1)
         move(t1,t3)
+        render()
 
 
 if __name__ == '__main__':
